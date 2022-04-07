c1f5756a4d1e2f65beafd3f391959df54eba6726
MBA <mbasonny@gmail.com>
2022-04-07 16:57:42 +0100

    OffreServiceImpl OK

--- src/main/java/repositories/OffreRepository.java
+++ src/main/java/repositories/OffreRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OffreRepository extends JpaRepository<Offre, String> {
-
+    Offre findByIntitule(String intitule);
 }
--- src/main/java/services/OffreServiceImpl.java
+++ src/main/java/services/OffreServiceImpl.java
@@ -2,10 +2,13 @@ package services;
 
 import dto.OffreRequestDTO;
 import dto.OffreResponseDTO;
+import entities.Offre;
 import mappers.OffreMapper;
 import repositories.OffreRepository;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class OffreServiceImpl implements  OffreService{
 
@@ -19,26 +22,48 @@ public class OffreServiceImpl implements  OffreService{
 
     @Override
     public OffreResponseDTO getOne(String id) {
-        return null;
+        Offre offre = offreRepository.findById(id).get();
+        return offreMapper.offreToOffreResponseDTO(offre);
     }
 
     @Override
     public List<OffreResponseDTO> getAll() {
-        return null;
+        List<Offre> offres = offreRepository.findAll();
+        return offres.stream()
+                .map(offre -> offreMapper.offreToOffreResponseDTO(offre))
+                .collect(Collectors.toList());
     }
 
     @Override
     public OffreResponseDTO save(OffreRequestDTO offreRequestDTO) {
-        return null;
+        Offre offre = offreMapper.offreRequestDTOTOOffre(offreRequestDTO);
+        offre.setId(UUID.randomUUID().toString());
+        try {
+            return offreMapper.offreToOffreResponseDTO(offreRepository.save(offre));
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            return null;
+        }
     }
 
     @Override
     public OffreResponseDTO update(OffreRequestDTO offreRequestDTO) {
-        return null;
+        Offre offre = offreMapper.offreRequestDTOTOOffre(offreRequestDTO);
+        try {
+            return offreMapper.offreToOffreResponseDTO(offreRepository.save(offre));
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            return null;
+        }
     }
 
     @Override
     public void delete(String id) {
-
+        try {
+            Offre offre = offreRepository.findById(id).get();
+            offreRepository.delete(offre);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }

eee0e49392e62624d694a6a54705aec39944740f
MBA <mbasonny@gmail.com>
2022-04-07 17:11:34 +0100

    PackServiceImpl OK

--- src/main/java/services/PackServiceImpl.java
+++ src/main/java/services/PackServiceImpl.java
@@ -2,10 +2,14 @@ package services;
 
 import dto.PackRequestDTO;
 import dto.PackResponseDTO;
+import entities.Offre;
+import entities.Pack;
 import mappers.PackMapper;
 import repositories.PackRepository;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class PackServiceImpl implements PackService{
 
@@ -19,26 +23,46 @@ public class PackServiceImpl implements PackService{
 
     @Override
     public PackResponseDTO getOne(String id) {
-        return null;
+        return packMapper.packToPackResponseDTO(packRepository.findById(id).get());
     }
 
     @Override
     public List<PackResponseDTO> getAll() {
-        return null;
+        return packRepository.findAll().stream()
+                .map(pack -> packMapper.packToPackResponseDTO(pack))
+                .collect(Collectors.toList());
     }
 
     @Override
     public PackResponseDTO save(PackRequestDTO packRequestDTO) {
-        return null;
+        Pack pack = packMapper.packRequestDTOTOPack(packRequestDTO);
+        pack.setId(UUID.randomUUID().toString());
+        try {
+            return packMapper.packToPackResponseDTO(packRepository.save(pack));
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+            return null;
+        }
     }
 
     @Override
     public PackResponseDTO update(PackRequestDTO packRequestDTO) {
-        return null;
+        Pack pack = packMapper.packRequestDTOTOPack(packRequestDTO);
+        try {
+            return packMapper.packToPackResponseDTO(packRepository.save(pack));
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+            return null;
+        }
     }
 
     @Override
     public void delete(String id) {
-
+        try {
+            Pack pack = packRepository.findById(id).get();
+            packRepository.delete(pack);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
